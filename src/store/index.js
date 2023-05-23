44c137d47382e32eca9da2e98e624d770516ddb1
Lucas Mira <lucas-mira1@hotmail.com>
2023-05-23 17:28:22 -0300

    speakers 1.0

--- src/store/index.js
+++ src/store/index.js
@@ -57,7 +57,45 @@ export default createStore({
         img1: require('@/assets/productImgs/xx59img1.svg'),
         img2: require('@/assets/productImgs/xx59img2.svg'),
         img3: require('@/assets/productImgs/xx59img3.svg'),
-      }
+      },
+      {
+        id: 4,
+        name: 'ZX9 SPEAKER',
+        isNewProduct: true,
+        description: 'Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.',
+        price: 4500,
+        features: 'Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.',
+        inTheBox: [
+          { qty: 2, text: 'Speaker Unit'},
+          { qty: 2, text: 'Speaker Cloth Panel'},
+          { qty: 1, text: 'User Manual'},
+          { qty: 1, text: '3.5mm 5m Audio Cable'},
+          { qty: 1, text: '10m Optical Cable'}
+        ],
+        mainImg: require('@/assets/productImgs/zx9MainImg.svg'),
+        img1: require('@/assets/productImgs/zx9img1.svg'),
+        img2: require('@/assets/productImgs/zx9img2.svg'),
+        img3: require('@/assets/productImgs/zx9img3.svg'),
+      },
+      {
+        id: 5,
+        name: 'ZX7 SPEAKER',
+        isNewProduct: false,
+        description: 'Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.',
+        price: 3500,
+        features: 'Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage. The ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.',
+        inTheBox: [
+          { qty: 2, text: 'Speaker Unit'},
+          { qty: 2, text: 'Speaker Cloth Panel'},
+          { qty: 1, text: 'User Manual'},
+          { qty: 1, text: '3.5mm 5m Audio Cable'},
+          { qty: 1, text: '10m Optical Cable'}
+        ],
+        mainImg: require('@/assets/productImgs/zx7MainImg.svg'),
+        img1: require('@/assets/productImgs/zx7img1.svg'),
+        img2: require('@/assets/productImgs/zx7img2.svg'),
+        img3: require('@/assets/productImgs/zx7img3.svg'),
+      },
     ]
   },
   getters: {
--- src/views/SpeakersView.vue
+++ src/views/SpeakersView.vue
@@ -1,9 +1,53 @@
 <template>
-    <div>speakers</div>
+    <div>
+        <h2 class="headphones-title">
+            speakers
+        </h2>
+        <div class="headphones-body">
+            <ProductCardComponent v-for="(product, idx) in products" :key="idx" :isReversed="idx % 2 != 0" :product="product"/>
+            <CategoriesComponent />
+            <BrandInfoComponent style="margin-top: 15vh;"/>
+        </div>
+    </div>
 </template>
 
 <script>
+import ProductCardComponent from '@/components/ProductCardComponent.vue';
+import CategoriesComponent from '@/components/CategoriesComponent.vue';
+import BrandInfoComponent from '@/components/BrandInfoComponent.vue';
+
 export default {
-    name: 'SpeakersView'
+    name: 'SpeakersView',
+    components:{
+        ProductCardComponent,
+        CategoriesComponent,
+        BrandInfoComponent
+    },
+    data () {
+        return {
+            products: [
+                this.$store.state.products[3],
+                this.$store.state.products[4]
+            ]
+        }
+    }
 }
 </script>
+
+<style scoped>
+.headphones-title{
+    background-color: #1C1C1C;
+    text-align: center;
+    padding: 6vh 0;
+    color: white;
+}
+.headphones-body{
+    padding: 5vh 11.5%;
+}
+
+@media screen and (max-width: 850px){
+    .headphones-body{
+        padding: 5vh 6vw;
+    }
+}
+</style>
