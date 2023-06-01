4b4bed8d9e5943c1191af29d9119b845fbc25304
Lucas Mira <lucas-mira1@hotmail.com>
2023-06-01 17:37:05 -0300

    checkout 0.2

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,40 +1,62 @@
 <template>
     <div>
-        <p class="input-title">{{ title }}</p>
-        <input v-if="type == 'tel'" type="tel" v-mask="['(##) ####-####', '(##) #####-####']" :placeholder="placeholder" class="input-value">
-        <input v-else-if="type == 'zip'" type="tel" v-mask="'## ###-###'" :placeholder="placeholder" class="input-value">
-        <input v-else type="text" :placeholder="placeholder" class="input-value">
+        <div class="input-title-container">
+            <p class="input-title" :class="{ 'input-title-error': errorMessage }">{{ title }}</p>
+            <p v-if="errorMessage" class="input-title input-title-error">{{ errorMessage }}</p>
+        </div>
+
+        <input v-on:change="event => setValue(event.target.value)" v-if="type == 'tel'" type="tel"
+            v-mask="['(##) ####-####', '(##) #####-####']" :placeholder="placeholder" class="input-value"
+            :class="{ 'input-value-error': errorMessage }">
+        <input v-on:change="event => setValue(event.target.value)" v-else-if="type == 'zip'" type="tel"
+            v-mask="'## ###-###'" :placeholder="placeholder" class="input-value"
+            :class="{ 'input-value-error': errorMessage }">
+        <input v-on:change="event => setValue(event.target.value)" v-else type="text" :placeholder="placeholder"
+            class="input-value" :class="{ 'input-value-error': errorMessage }">
     </div>
 </template>
 
 <script>
 export default {
     name: 'InputComponent',
-    props: ['title', 'placeholder', 'type']
+    props: ['title', 'placeholder', 'type', 'errorMessage', 'setValue']
 }
 </script>
 
 <style scoped>
 .input-title {
     font-weight: bold;
+
+}
+
+.input-title-container {
     margin-bottom: 0.5vh;
+    display: flex;
+    justify-content: space-between;
+}
+
+.input-title-error {
+    color: red;
 }
 
 .input-value {
-    width: 100%;
+    width: 95%;
     border: 1px solid #CFCFCF;
     border-radius: 8px;
-    padding: 1vh 0 1vh 1.5vw;
+    padding: 1vh 0 1vh 5%;
     font-family: 'Manrope';
     font-weight: 700;
     font-size: 1.2em;
     line-height: 1.2;
     color: #000000;
-    opacity: 0.4;
 
 }
 
 .input-value:focus {
-    outline: none;
+    outline: #D87D4A 1px solid;
+}
+
+.input-value-error{
+    outline: 1px solid red !important;
 }
 </style>
--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -7,26 +7,27 @@
                 <h3>CHECKOUT</h3>
                 <p class="subtitle checkout-subtitle">Billing Details</p>
                 <div class="checkout-input-line checkout-inputs-margin">
-                    <InputComponent placeholder="Alex" class="checkout-input-half" title="name"/>
-                    <InputComponent placeholder="Alex@gmail.com" class="checkout-input-half" title="Email adress"/>
+                    <InputComponent :errorMessage="nameMessage" :setValue="value => name = value" placeholder="Alex" class="checkout-input-half" title="name"/>
+                    <InputComponent :errorMessage="emailMessage" :setValue="value => email = value" placeholder="Alex@gmail.com" class="checkout-input-half" title="Email adress"/>
                 </div>
                 <div class="checkout-input-line checkout-inputs-margin">
-                    <InputComponent placeholder="(12)3456-7891" type="tel" class="checkout-input-half" title="Phone Number"/>
+                    <InputComponent :errorMessage="phoneMessage" :setValue="value => phone = value" placeholder="(12)3456-7891" type="tel" class="checkout-input-half" title="Phone Number"/>
                 </div>
                 
                 <p  class="subtitle  checkout-subtitle">shipping info</p>
-                <InputComponent class=" checkout-inputs-margin" placeholder="Rua das palmeiras" title="Address"/>
+                <InputComponent :errorMessage="addressMessage" :setValue="value => address = value" class=" checkout-inputs-margin" placeholder="Rua das palmeiras" title="Address"/>
                 <div class="checkout-input-line  checkout-inputs-margin">
-                    <InputComponent type="zip" class="checkout-input-half" title="zip code" placeholder="12 323-230"/>
-                    <InputComponent class="checkout-input-half" title="city" placeholder="São Paulo"/>
+                    <InputComponent :errorMessage="zipMessage" :setValue="value => zip = value" type="zip" class="checkout-input-half" title="zip code" placeholder="12 323-230"/>
+                    <InputComponent :errorMessage="cityMessage" :setValue="value => city = value" class="checkout-input-half" title="city" placeholder="São Paulo"/>
                 </div>
                 <div class="checkout-input-line  checkout-inputs-margin">
-                    <InputComponent class="checkout-input-half" title="country" placeholder="Brasil"/>
+                    <InputComponent :errorMessage="countryMessage" :setValue="value => country = value" class="checkout-input-half" title="country" placeholder="Brasil"/>
                 </div>
             </div>
 
             <div class="checkout-rigth-container">
                 <h6>Resumo</h6>
+                <button v-on:click="save()">salvar</button>
             </div>
 
         </div>
@@ -41,9 +42,57 @@ export default {
     components:{
         InputComponent
     },
+    data() {
+        return {
+            name: '',
+            nameMessage: '',
+            email: '',
+            emailMessage: '',
+            phone: '',
+            phoneMessage: '',
+            address: '',
+            addressMessage: '',
+            zip: '',
+            zipMessage: '',
+            city: '',
+            cityMessage: '',
+            country: '',
+            countryMessage: ''
+        }
+    },
+    watch:{
+        name (newValue) {if (newValue) this.nameMessage = ''},
+        email (newValue) {if (newValue) this.emailMessage = ''},
+        phone (newValue) {if (newValue) this.phoneMessage = ''},
+        address (newValue) {if (newValue) this.addressMessage = ''},
+        zip (newValue) {if (newValue) this.zipMessage = ''},
+        city (newValue) {if (newValue) this.cityMessage = ''},
+        country (newValue) {if (newValue) this.countryMessage = ''},
+    },
     methods: {
         returnPage() {
             this.$router.go(-1)
+        },
+        save () {
+            if (!this.name) this.nameMessage = 'Campo obrigatorio'
+            else if (this.name.length < 5) this.nameMessage = 'Nome completo'
+
+            const pattern = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i 
+            if (!this.email) this.emailMessage = 'Campo obrigatorio'
+            else if (!this.email.match(pattern)) this.emailMessage = 'Insira um email valido'
+
+            if (!this.phone) this.phoneMessage = 'Campo obrigatorio'
+            else if (this.phone.length < 14) this.phoneMessage = 'Insira o numero completo'
+
+            if (!this.address) this.addressMessage = 'Campo obrigatorio'
+            else if (this.address.length < 6) this.addressMessage = 'Insira o endereço completo'
+
+            if (!this.zip) this.zipMessage = 'Campo obrigatorio'
+            else if (this.zip.length < 10) this.zipMessage = 'Insira o CEP completo'
+
+            if (!this.city) this.cityMessage = 'Campo obrigatorio'
+
+            if (!this.country) this.countryMessage = 'Campo obrigatorio'
         }
     },
 }
@@ -116,5 +165,17 @@ export default {
     }
 }
 
-@media screen and (max-width: 850px) {}
+@media screen and (max-width: 550px) {
+    .checkout-input-line{
+        flex-direction: column;
+        gap: 1vh;
+    }
+    .checkout-input-half{
+        width: 100%;
+    }
+    .checkout-left-container{
+        padding: 4vh 4vw;
+        width: 92%;
+    }
+}
 </style>
